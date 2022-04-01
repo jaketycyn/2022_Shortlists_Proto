1d8bb96dfc1dda0cbed9b5809bd3ba799fc2ad9a
Jake TyCyn <jaketycyn@gmail.com>
2022-04-01 10:00:48 -0700

    AD: Global & Theme provider

---
+++ client/src/assets/utils/theme.js
@@ -0,0 +1,40 @@
+const theme = {
+  colors: {
+    //future main theme
+    main: "#db0000",
+    mainDark: "#831010",
+    mainLight: "#E14444",
+    mainLighter: "#DE2A0B",
+    mainBlack: "#000000",
+    darkMain: "#332E30",
+    darkDarker: "#2b2929",
+    darkLighter: "#999999",
+    secondary: "#55688A",
+    secondaryLighter: "#7B89A4",
+    //whites
+    mainWhite: "#EEF0F3",
+    //blues
+    mainBlue: "#1A73E8",
+    //greens
+    mainGreen: "#188038",
+    //yellows
+    mainYellow: "#E3B015",
+    //oranges
+    mainOrange: "#D6473F",
+
+    //text
+    textColor: "#202124",
+    //errors
+    errorRed: "#D93025",
+    shadow: "rgba(0,0,0,.2)",
+  },
+
+  mediaQueries: {
+    sm: `only screen and (min-width: 12em)`,
+    md: `only screen and (min-width: 48em)`,
+    lg: `only screen and (min-width: 62em)`,
+    xl: `only screen and (min-width: 75em)`,
+  },
+};
+
+export default theme;
