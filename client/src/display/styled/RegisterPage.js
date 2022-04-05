79b992e2adaef964f6e2a5f8488f81a99597b989
Jake TyCyn <jaketycyn@gmail.com>
2022-04-05 00:56:23 -0700

    RF: Styled RegisterPage

--- client/src/display/styled/RegisterPage.js
+++ client/src/display/styled/RegisterPage.js
@@ -1,39 +1,5 @@
-// const Wrapper = styled.section`
-//   /* display: grid;
-//   align-items: center;
-//   .logo {
-//     display: block;
-//     margin: 0 auto;
-//     margin-bottom: 1.38rem;
-//   }
-//   .form {
-//     max-width: 400px;
-//     border-top: 5px solid var(--primary-500);
-//   }
-
-//   h3 {
-//     text-align: center;
-//   }
-//   p {
-//     margin: 0;
-//     margin-top: 1rem;
-//     text-align: center;
-//   }
-//   .btn {
-//     margin-top: 1rem;
-//   }
-//   .member-btn {
-//     background: transparent;
-//     border: transparent;
-//     color: var(--primary-500);
-//     cursor: pointer;
-//     letter-spacing: var(--letterSpacing);
-//   } */
-// `;
-
-// import React from "react";
-
 import styled, { css } from "styled-components";
+//will need to look into these CSS features used below and how to relate to global/theme
 
 export const CardWrapper = styled.div`
   overflow: hidden;
