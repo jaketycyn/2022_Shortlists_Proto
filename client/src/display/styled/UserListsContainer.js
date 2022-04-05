7ed8157af987c9fe8753ca49669f4308033531d2
Jake TyCyn <jaketycyn@gmail.com>
2022-04-05 00:56:59 -0700

    ST: Basic Grid setup for Displaying UserLists

--- client/src/display/styled/UserListsContainer.js
+++ client/src/display/styled/UserListsContainer.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 const Wrapper = styled.article`
   //grid
-  display: grid;
+
   grid-gap: 1em;
   background: ${(props) => props.theme.colors.mainPink};
   /* margin: 10px 50px 20px; */
@@ -11,52 +11,18 @@ const Wrapper = styled.article`
 
   .list-input {
     background: ${(props) => props.theme.colors.mainBlue};
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+
+    @media (max-width: 1200px) {
+      display: grid;
+      grid-template-columns: repeat(1, 1fr);
+    }
   }
 
-  //basesetup/paramets of cards
-
-  /* @media (min-width: 576px) {
-    grid-template-columns: 3fr 1fr 1fr;
-  }
-  @media (min-width: 992px) {
-    grid-template-columns: 3fr 1fr 1fr;
-  }
-  @media (min-width: 1120px) {
-    grid-template-columns: 3fr 1fr 1fr;
-  } */
-
-  //generic hover
   &:hover {
     box-shadow: 0 1px 16px 0 rgba(0, 0, 0, 0.55);
   }
-  /* 
-
-  .main {
-    grid-area: main;
-    font-size: 2rem;
-    margin: 5px 5px;
-    background: ${(props) => props.theme.colors.mainYellow};
-    place-self: center stretch;
-  }
-  .secondary {
-    grid-column: 2 / 4;
-    display: grid;
-    grid-template-columns: subgrid;
-    grid-template-areas: "share delete";
-    margin: 5px 5px;
-    place-self: center stretch;
-  }
-
-  .share {
-    //background: ${(props) => props.theme.colors.mainBlue};
-    height: 5em;
-    width: 5em;
-  }
-  .delete {
-    //background: ${(props) => props.theme.colors.errorRed};
-    height: 5em;
-    width: 5em;
-  } */
 `;
 
 export default Wrapper;
